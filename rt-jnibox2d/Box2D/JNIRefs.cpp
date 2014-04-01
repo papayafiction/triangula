8657b942593b1e80cc8422a881a00a2e9cab892f
Stefan Mayer <stevijo.mayer@googlemail.com>
2014-04-02 00:49:06 +0200

    Cleaned up some native Code

--- rt-jnibox2d/Box2D/JNIRefs.cpp
+++ rt-jnibox2d/Box2D/JNIRefs.cpp
@@ -14,24 +14,14 @@ jobject globalRef [MAX_GLOBAL_REFS];
 
 jobject MakeGlobalRef(JNIEnv* env, jobject obj) {
 
-	// loop through global refs and see if this one is already there
 	for(int i = 0 ; i < MAX_GLOBAL_REFS ; i++)
 	{
 		if(globalRef[i] == obj) {
 			printf("re-using global ref %d", (int)obj);
 			return obj;
-		}
-	}
-
-	// loop through global refs and find unused pos (NULL)
-	for(int i = 0 ; i < MAX_GLOBAL_REFS ; i++)
-	{
-		if(globalRef[i] == 0) {
-//			printf("creating java global reference %d @ %d \n", obj, i);
-
+		} else if(globalRef[i] == 0) {
 			jobject jglobal = env->NewGlobalRef(obj);
 
-			// add global ref to reflist
 			globalRef[i] = jglobal;
 
 			return jglobal;
@@ -47,10 +37,8 @@ jobject MakeGlobalRef(JNIEnv* env, jobject obj) {
 }
 
 void DeleteAllGlobalRefs(JNIEnv* env) {
-	// delete global refs
 	for(int i = 0 ; i < MAX_GLOBAL_REFS ; i++) {
 		if(globalRef[i] != 0) {
-//			printf("deleting Java global ref %d from pos #%d\n", globalRef[i], i);
 			env->DeleteGlobalRef(globalRef[i]);
 			globalRef[i] = 0;
 		}
@@ -60,10 +48,10 @@ void DeleteAllGlobalRefs(JNIEnv* env) {
 void DeleteGlobalRef(JNIEnv* env, jobject obj) {
 	for(int i = 0 ; i < MAX_GLOBAL_REFS ; i++) {
 		if(globalRef[i] == obj) {
-//			printf("\tdeleting Java global ref %d from pos #%d\n", globalRef[i], i);
 			env->DeleteGlobalRef(globalRef[i]);
 
 			globalRef[i] = 0;
+			return;
 		}
 	}
 }
