8e04a8108a39b1bb4cfce5a2facd264c2e7322e6
Stefan Mayer <stevijo.mayer@googlemail.com>
2014-04-04 16:48:09 +0200

    Bombs now kill you

--- src/de/sopamo/triangula/android/game/GameImpl.java
+++ src/de/sopamo/triangula/android/game/GameImpl.java
@@ -29,22 +29,20 @@ import de.sopamo.triangula.android.game.models.Player;
 import de.sopamo.triangula.android.game.mechanics.Rewindable;
 import de.sopamo.triangula.android.geometry.*;
 import de.sopamo.triangula.android.levels.Level;
-import de.sopamo.triangula.android.levels.OnlineLevel;
-import de.sopamo.triangula.android.levels.Starter;
 import de.sopamo.triangula.android.particles.Particle;
 import org.jbox2d.collision.AABB;
 import org.jbox2d.common.Vec2;
-import org.jbox2d.dynamics.World;
 import android.util.Log;
 import de.sopamo.box2dbridge.Box2DFactory;
 import de.sopamo.box2dbridge.IBody;
 import de.sopamo.box2dbridge.IWorld;
 import de.sopamo.box2dbridge.jbox2d.JBox2DWorld;
 import de.sopamo.box2dbridge.jnibox2d.JNIBox2DWorld;
-import de.sopamo.triangula.android.MainActivity;
+import de.sopamo.triangula.android.GameActivity;
 
 public class GameImpl implements GameInterface {
 
+    public static  final int   TIME_TO_RESTART = 500;
 	private static final float TIME_STEP = 1f / 60f;
 	private static final int   ITERATIONS = 5;
 
@@ -53,6 +51,7 @@ public class GameImpl implements GameInterface {
 
     private static GameImpl instance;
 
+    private boolean reinit;
 
     private static IBody playerBody;
     private static Player player;
@@ -68,6 +67,7 @@ public class GameImpl implements GameInterface {
     // Debug stuff
     long nanoTime;
     long lastClick = 0;
+    long respawnTime;
     float fps;
     int frames;
 
@@ -87,6 +87,7 @@ public class GameImpl implements GameInterface {
     public void init(InputHandler handler, Level level) {
         this.level = level;
         this.handler = handler;
+        respawnTime = 0;
 		// density of dynamic bodies
 		float density = 1;
 		
@@ -95,7 +96,9 @@ public class GameImpl implements GameInterface {
 						new Vec2(-50, -50), 
 						new Vec2( 50,  50)
 					);
-
+        if(world == null) {
+            world = Box2DFactory.newWorld();
+        }
 
         // World setup
         Vec2 gravity = new Vec2(0, -9.8f);
@@ -115,16 +118,22 @@ public class GameImpl implements GameInterface {
     }
 
 	public void destroy() {
-		// in case we are using JNIBox2D, this
-		// is very important! otherwise we end up with memory leaks.
-		// world.destroy will recursively destroy all its attached content
-		world.destroy();
+        // in case we are using JNIBox2D, this
+        // is very important! otherwise we end up with memory leaks.
+        // world.destroy will recursively destroy all its attached content
+        world.destroy();
+        world = null;
+        rewindables = new ArrayList<Rewindable>();
+        entities = new ArrayList<Entity>();
+        gsl = new ArrayList<GameShape>();
+        pl = new ArrayList<Particle>();
 	}
 
 	@Override
 	public void drawFrame() {
         // Draw game shapes
-		for(GameShape gs : gsl) {
+		for(int i=0;i<gsl.size();i++) {
+            GameShape gs = gsl.get(i);
 			gs.draw();
 		}
 
@@ -135,18 +144,31 @@ public class GameImpl implements GameInterface {
         }
 	}
 
+    public void reinit() {
+        reinit = true;
+    }
+
 	@Override
 	public void gameLoop() {
 		if(world == null) {
 			Log.e("pg", "World not initialized");
 			return;
 		}
+        if(reinit) {
+            respawnTime+=1000l/60;
+            if(respawnTime>=TIME_TO_RESTART) {
+                reinit = false;
+                destroy();
+                init(handler,level);
+            }
+        }
 
         handler.update();
 
         /** ## DEBUG ## **/
 		frames++;
 		long elap = System.currentTimeMillis() - nanoTime;
+
 		if(elap > 1000) {
 			// update info every second
 			fps = frames / ((float)elap / 1000f);
@@ -154,7 +176,7 @@ public class GameImpl implements GameInterface {
 			frames = 0;
 
 			String engine = (world instanceof JBox2DWorld ? "JBox2D" : world instanceof JNIBox2DWorld ? "JNIBox2D": "unknown");
-			MainActivity.setStatus(engine + ", fps: " + fps);
+			GameActivity.setStatus(engine + ", fps: " + fps);
 		}
         /** ## END DEBUG ## **/
 
@@ -182,7 +204,7 @@ public class GameImpl implements GameInterface {
             Entity entity = entities.get(i);
             entity.update();
         }
-        
+
         world.step(TIME_STEP, ITERATIONS);
         world.sync();
 	}
